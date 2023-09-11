6cc11d65f6748de7c5890d986acfdbb382063ef2
Tom Kalmijn <tkalmijn@servilocker.com>
2023-09-11 18:33:38 +0200

    tweaking daemon behavior

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # hue-dmx-controller
+# author: Tom Kalmijn
+#
+# This is a simple script to sync a single DMX fixture with a hue bulb for the ENTTEC OPEN DMX USB dongle.
+#
+# When succesfully installed it will enable you to control the DMX fixture from the Philips Hue app.
+#
+# Note: ENTTEC OPEN DMX PRO is not supported (please contact me via github for feature requests)
+#
--- hue-dmx.py
+++ hue-dmx.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import sys
 import time
 import json
 import daemon
@@ -7,83 +8,119 @@ from pylibftdi import Device, Driver
 from daemon import pidfile
 from dotenv import load_dotenv
 import requests
+import signal
 
+# initialize variables from config file (.env)
 load_dotenv()
-logger = logging.getLogger('hue-dmx')
-logger.setLevel(logging.INFO)
 
-log_file_handler = logging.FileHandler(os.getenv('LOG_FILE'))
-log_file_handler.setLevel(logging.INFO)
+PID_FILE = os.getenv('PID')
+WORK_DIR = os.getenv('WORK_DIR')
+LOG_FILE = os.getenv('LOG_FILE')
 
-format_string = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-formatter = logging.Formatter(format_string)
+HUE_API_KEY = os.getenv('HUE_API_KEY')
+HUE_LAMP_ID = os.getenv('HUE_LAMP_ID')
+HUE_BRIDGE_IP = os.getenv('HUE_BRIDGE_IP')
+HUE_POLL_SECONDS = float(os.getenv('HUE_POLL_SEC'))
 
-log_file_handler.setFormatter(formatter)
-logger.addHandler(log_file_handler)
+DMX_ADDRESS = int(os.getenv('DMX_ADDRESS'))
 
-dmx_brightness = -1
+brightness = -1
+# brightness: the last dimming level is cached for optimization. DMX data is sent only when there is a change
+# on the side of the Philips Hue bulb. This assumes that your DMX fixtures have a feature to HOLD the last setting
+# (instead of blacking out). If your fixture does not have this option or you want to turn off your DMX fixtures
+# when the script exits, then you need to add code to keep sending the DMX data (e.g. 44 times per second).
+
+dmx_data = bytearray(513)
+# 513: one start byte (0x00) plus 512 bytes of channel data
+# dmx_data is automatically filled with zeros, incidentally also correctly setting the start byte.
+# According to DMX512, when sending a message to a fixture, we need to repeat the untouched DMX
+# channels. For this reason channel data is buffered in dmx_data.
+
+logger = logging.getLogger()
+file_logger = logging.FileHandler(LOG_FILE)
+
+
+def init_logger():
+    global logger, file_logger
+    logger.setLevel(logging.INFO)
+    file_logger.setLevel(logging.INFO)
+    file_logger.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(file_logger)
 
 
 def track_hue_lamp_and_update_dmx():
-    global dmx_brightness
-    hue_api_key = os.getenv('HUE_API_KEY')
-    hue_lamp_id = os.getenv('HUE_LAMP_ID')
-    hue_bridge_ip = os.getenv('HUE_BRIDGE_IP')
+    logger.info("Starting Hue-DMX Daemon...")
+    global brightness
 
     while True:
-        response = requests.get(f"http://{hue_bridge_ip}/api/{hue_api_key}/lights/{hue_lamp_id}")
+        response = requests.get(f"http://{HUE_BRIDGE_IP}/api/{HUE_API_KEY}/lights/{HUE_LAMP_ID}")
         if response.status_code == 200:
             response_obj = json.loads(response.text)
-            dmx_brightness = response_obj['state']['bri'] if response_obj['state']['on'] else 0
-            update_dmx()
-        time.sleep(0.5)
+            new_brightness = response_obj['state']['bri'] if response_obj['state']['on'] else 0
+            if brightness != new_brightness:
+                brightness = new_brightness
+                data = bytearray([brightness])  # specific to my situation, updates one DMX channel at address 1
+                update_dmx(DMX_ADDRESS, bytes(data))
+        time.sleep(HUE_POLL_SECONDS)
 
 
-def send_dmx_packet(dev: Device, data: bytes):
+def send_dmx_packet(device: Device, data: bytes):
     # reset dmx channel
-    dev.ftdi_fn.ftdi_set_bitmode(1, 0x01)  # set break
-    dev.write(b'\x00')
+    device.ftdi_fn.ftdi_set_bitmode(1, 0x01)  # set break
+    device.write(b'\x00')
     time.sleep(0.001)
-    dev.write(b'\x01')
-    dev.ftdi_fn.ftdi_set_bitmode(0, 0x00)  # release break
-    dev.flush()
+    device.write(b'\x01')
+    device.ftdi_fn.ftdi_set_bitmode(0, 0x00)  # release break
+    device.flush()
 
-    dev.ftdi_fn.ftdi_set_line_property(8, 2, 0)
-    dev.baudrate = 250000
-    dev.write(bytes(data))
-    dev.flush()
-    dev.close()
+    device.ftdi_fn.ftdi_set_line_property(8, 2, 0)
+    device.baudrate = 250000
+    device.write(bytes(data))
+    device.flush()
+    device.close()
 
 
-def update_dmx():
+def update_dmx(address: int, data: bytes):
+    global dmx_data
     driver = Driver()
     devices = driver.list_devices()
 
     if not devices:
-        logger.error("No FTDI devices found")
+        logger.error("No FTDI devices found.")
         return
-
-    data = bytearray(513)
-    data[0] = 0
-    data[1] = dmx_brightness
 
     for device in devices:
         manufacturer, description, serial = device
         logger.debug(f"Manufacturer: {manufacturer}, Description: {description}, Serial: {serial}")
 
         with Device(serial) as dev:
-            send_dmx_packet(dev, data)
+            logger.info(f"Updating dmx address {address}")
+            dmx_data[address:address + len(data)] = data
+            # address equals offset because DMX addresses start with 1 skipping the start byte in the data packet.
+            send_dmx_packet(dev, dmx_data)
+
+
+def shutdown(signum, frame):
+    logger.info('shutting down')
+    exit(0)
 
 
 def start_daemon():
     with daemon.DaemonContext(
-            working_directory=os.getenv('WORK_DIR'),
             umask=0o002,
-            pidfile=pidfile.TimeoutPIDLockFile(os.getenv('PID')),
-    ):
+            working_directory=WORK_DIR,
+            files_preserve=[file_logger.stream],
+            pidfile=pidfile.PIDLockFile(PID_FILE)):
+        signal.signal(signal.SIGTERM, shutdown)
+        signal.signal(signal.SIGINT, shutdown)
+        init_logger()
         track_hue_lamp_and_update_dmx()
 
 
 if __name__ == "__main__":
-    # start_daemon()
-    track_hue_lamp_and_update_dmx()
+    if os.getenv('DAEMONIZE') == 'true':
+        start_daemon()
+    else:
+        track_hue_lamp_and_update_dmx()
+
+
