b2762ad3b33d1a6149d1dc90edef5fafbf1712bd
Tom Kalmijn <tkalmijn@servilocker.com>
2023-09-21 09:58:10 +0200

    boy scouting

--- AdjSaberSpotWW.py
+++ AdjSaberSpotWW.py
@@ -1,23 +1,19 @@
+import math
+from typing import Dict, Any
+
 from DmxFixture import DmxFixture
 
 
 class AdjSaberSpotWW(DmxFixture):
-    on = False
-    brightness = 0 # expected to be between 0 and 255
 
-    def get_dmx_message(self, hue_light_info: object) -> bytes:
-        if 'on' in hue_light_info:
-            self.on = hue_light_info['on']['on']
+    def get_dmx_message(self, hue_light_info: Dict[str, Any]) -> bytes:
+        if not 'on' in hue_light_info or not hue_light_info['on']['on']:
+            return bytes([0])
 
-        if 'dimming' in hue_light_info:
-            self.brightness = int(hue_light_info['dimming']['brightness'])
-            if self.brightness > 254:
-                self.brightness = 254
-            if self.brightness < 0:
-                self.brightness = 0
+        dim_level = math.ceil(hue_light_info['dimming']['brightness']) if 'dimming' in hue_light_info else 255
+        if dim_level > 254:
+            dim_level = 254
+        if dim_level < 0:
+            dim_level = 0
 
-        dmx_dim_level = self.brightness if self.on else 0
-        return bytes([dmx_dim_level])
-
-    def get_state(self) -> str:
-        return f"on={self.on}  brightness={self.brightness}"
+        return bytes([dim_level])
